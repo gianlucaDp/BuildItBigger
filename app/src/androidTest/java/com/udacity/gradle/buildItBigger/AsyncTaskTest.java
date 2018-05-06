6a08487a530e1f81ae9ca9b2adde5c4fe1a644bb
Gianluca De Prisco <mistergdp@gmail.com>
2018-05-06 17:07:19 +0200

    Added CountDownLatch to test + moved banner string in free flavor

--- app/src/androidTest/java/com/udacity/gradle/buildItBigger/AsyncTaskTest.java
+++ app/src/androidTest/java/com/udacity/gradle/buildItBigger/AsyncTaskTest.java
@@ -7,15 +7,19 @@ import com.udacity.gradle.builditbigger.EndPointAsyncTask;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.concurrent.CountDownLatch;
+
 import static junit.framework.Assert.assertNotNull;
 import static junit.framework.Assert.assertTrue;
 
 @RunWith(AndroidJUnit4.class)
 public class AsyncTaskTest {
-
+    private CountDownLatch signal;
     @Test
-    public void testJokeIsRetrieved() {
+    public void testJokeIsRetrieved() throws InterruptedException {
+        signal = new CountDownLatch(1);
         new TestAsyncTask().execute();
+        signal.await();
     }
 
     private class TestAsyncTask extends EndPointAsyncTask {
@@ -26,6 +30,7 @@ public class AsyncTaskTest {
             if (result != null) {
                 assertTrue("The joke string is empty", !result.isEmpty());
             }
+            signal.countDown();
         }
     }
 }
